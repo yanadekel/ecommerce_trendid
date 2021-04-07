a648d19479d489476b2aca2900fe953026f394e8
yanadekel <75662763+yanadekel@users.noreply.github.com>
2021-04-07 19:01:26 +0300

    logo chenge

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -4,8 +4,7 @@ import React from 'react';
 export default function Logo() {
   return (
     <div className="logoContainer">
-      <h1 className="app-logo">TREND!D
-      !</h1>
+      <h1 className="app-logo">TREND!D</h1>
     </div>
   )
 }
