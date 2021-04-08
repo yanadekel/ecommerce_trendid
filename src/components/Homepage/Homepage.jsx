42c995cbbe04a1fd671572c8a77955174e007361
yanadekel <75662763+yanadekel@users.noreply.github.com>
2021-04-08 10:32:26 +0300

    finel

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -9,9 +9,7 @@ import Slider from "react-slick";
 
 export default function HomePage() {
   const [GenderData, setGenderData] = useState([]);
-  // const [genderState, setGenderState] = useState("women");
-  // const [leftBtnState, setLeftBtnState]=useState("Men");
-  // const [rihgtBtnState, setRightBtnState]=useState("Kids");
+ 
 
   const settings = {
     dots: true,
@@ -21,7 +19,33 @@ export default function HomePage() {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
-    className: "slides"
+    className: "slides",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
 
   };
 
